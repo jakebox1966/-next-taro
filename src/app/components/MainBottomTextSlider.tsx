3ccf8a60db7dd81ad3fd25bd308a7dcdd75e913d
Beomseok <jakebox1966@gmail.com>
2024-01-24 23:13:34 +0900

    no message

--- src/app/components/MainBottomTextSlider.tsx
+++ src/app/components/MainBottomTextSlider.tsx
@@ -16,28 +16,30 @@ export default function MainBottomTextSlider(props: IMainBottomTextSliderProps) 
         '사자랑가자 타로 공유 이벤트',
         '사자랑가자 타로 공유 이벤트',
         '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
-        // '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
+        '사자랑가자 타로 공유 이벤트',
     ]
     return (
-        <div className="scroll-text bg-[#FF5A3F] text-[12px] py-6 font-[NotoSansKR400] mt-[81px] lg:mt-[70px]">
-            <div className="track">
-                {slides.map((item, index) => (
-                    <>
-                        <p key={index}>
-                            {item}
-                            &nbsp;<span>&nbsp;•</span>
-                        </p>
-                    </>
-                ))}
+        <>
+            <div className="scroll-text bg-[#FF5A3F] text-[12px] py-6 font-[NotoSansKR400] mt-[81px] lg:mt-[70px]">
+                <div className="track">
+                    {slides.map((item, index) => (
+                        <>
+                            <p key={index}>
+                                {item}
+                                &nbsp;<span>&nbsp;•</span>
+                            </p>
+                        </>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
