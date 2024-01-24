907c3e7d52d34d24d09ca65eb6c6d39dc69a6e06
Beomseok <jakebox1966@gmail.com>
2024-01-24 18:44:55 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
             {
                 source: '/api/:path*',
                 destination:
-                    'https://hits.seeyoufarm.com/api/count/keep/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+                    'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
             },
         ]
     },
