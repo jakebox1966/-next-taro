c3b265142edd03e65d1ff913b9b31f13c6080c83
Beomseok <jakebox1966@gmail.com>
2024-01-25 11:42:22 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -2,15 +2,23 @@
 const nextConfig = {
     // reactStrictMode: true,
     // swcMinify: true,
-    async rewrites() {
-        console.log('now rewrite')
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination:
+    //                 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+    //                 missing
+    //         },
+    //     ]
+    // },
+    async redirect() {
         return [
             {
                 source: '/api/:path*',
                 destination:
                     'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
-
-                permanent: false,
+                permanent: true,
             },
         ]
     },
