9d996f8896db21d60a6a90e8fe8b4019d72bbdb1
Beomseok <jakebox1966@gmail.com>
2024-01-25 14:34:47 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -5,22 +5,12 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/:path*',
+                source: '/api/test',
                 destination:
                     'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
             },
         ]
     },
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: '/api/:path*',
-    //             destination:
-    //                 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
-    //             permanent: false,
-    //         },
-    //     ]
-    // },
 }
 
 module.exports = nextConfig
--- src/app/api/test/route.tsx
+++ src/app/api/test/route.tsx
@@ -2,7 +2,8 @@ import { NextRequest } from 'next/server'
 
 export async function GET(req: NextRequest) {
     const result = await fetch(
-        // 'https://hits.seeyoufarm.com/api/count/keep/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+        // 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=http%3A%2F%2Flocalhost%3A3000&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+        // { method: 'GET', cache: 'no-store', headers: { 'Content-Type': 'image/svg+xml' } },
         'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
         { method: 'GET', cache: 'no-store', headers: { 'Content-Type': 'image/svg+xml' } },
     )
