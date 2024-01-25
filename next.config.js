477ce6092adfe5f05148a05459486f46e91aa868
Beomseok <jakebox1966@gmail.com>
2024-01-25 12:30:35 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true,
-    // swcMinify: true,
-    // async rewrites() {
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination:
+                    'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+            },
+        ]
+    },
+    // async redirects() {
     //     return [
     //         {
     //             source: '/api/:path*',
     //             destination:
     //                 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
-    //                 missing
+    //             permanent: false,
     //         },
     //     ]
     // },
-    async redirect() {
-        return [
-            {
-                source: 'api/:path*',
-                destination:
-                    'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
-                permanent: true,
-            },
-        ]
-    },
 }
 
 module.exports = nextConfig
