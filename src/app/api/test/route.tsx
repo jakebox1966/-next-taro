d384019b08a1da162eab2f28a57e4d03b6053ec0
Beomseok <jakebox1966@gmail.com>
2024-01-29 11:28:43 +0900

    no message

--- src/app/api/test/route.tsx
+++ src/app/api/test/route.tsx
@@ -15,7 +15,17 @@ export async function GET(req: NextRequest) {
         //         'Vercel-CDN-Cache-Control': 'max-age=0',
         //     },
         // },
-        'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+        // 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
+        // {
+        //     method: 'GET',
+        //     cache: 'no-store',
+        //     // headers: {
+        //     // 'Content-Type': 'image/svg+xml',
+        //     // 'CDN-Cache-Control': 'max-age=0',
+        //     // 'Vercel-CDN-Cache-Control': 'max-age=0',
+        //     // },
+        // },
+        'https://hits.seeyoufarm.com/api/count/keep/badge.svg?url=https%3A%2F%2Ftarot.quadhash.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false',
         {
             method: 'GET',
             cache: 'no-store',
